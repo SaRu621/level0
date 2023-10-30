01c5ab6a9d57e4fa9d24d33af5758db74c22f61a
SaRu621 <sagadulaev2011@mail.ru>
2023-10-30 18:54:43 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
     "fmt"
+    "github.com/nats-io/stan.go"
 )
 
 func main() {
-    fmt.Println("it work")
+    sc, err := stan.Connect("test-cluster", "client-1") //подключение к кластеру NATS Streaming
+
+    defer sc.Close()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    subscribtion, err := sc.Subscribe("my-channel", func(msg *stan.Msg) {
+        fmt.Printf("Получено сообщение: %s\n", string(msg.Data))
+    }, stan.DurableName("my-durable"))
+
+    defer subscribtion.Unsubscribe()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    ch := make(chan int) 
+    <- ch
 }
